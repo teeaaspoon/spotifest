c9794f151d26495012b8b11eed17bd0a4f28d57e
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-07-30 15:57:47 -0400

    changed user auth flow

--- client/src/components/auth/SpotifyLoginButton.js
+++ client/src/components/auth/SpotifyLoginButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 const SpotifyLoginButton = () => (
-  <a href="http://localhost:3001/auth/spotify/">Log In To Spotify</a>
+  <a href="http://localhost:3001/auth/spotify/">Connect to Spotify</a>
 )
 
 export default SpotifyLoginButton;
