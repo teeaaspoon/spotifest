e4aa2641aaaae187aa58a23399634618a7012abb
teeaaspoon <tommypoon36@gmail.com>
2018-07-31 17:42:50 +0000

    fixed migration error

--- db/migrate/20180730234201_add_index_to_artists.rb
+++ db/migrate/20180730234201_add_index_to_artists.rb
@@ -1,5 +1,5 @@
 class AddIndexToArtists < ActiveRecord::Migration[5.1]
   def change
-    add_index :artists, :spotify_artist_id
+    add_index :artists, :spotify_artist_id unless index_exists?(:artists, :spotify_artist_id)
   end
 end
