594909213b7fda3cd593455281b8efb7a07300ce
teeaaspoon <tommypoon36@gmail.com>
2018-08-06 19:15:17 -0400

    fixed build fail error by remaking react-native project

--- spotifestmobile/android/app/src/main/java/com/spotifestmobile/MainApplication.java
+++ spotifestmobile/android/app/src/main/java/com/spotifestmobile/MainApplication.java
@@ -3,7 +3,6 @@ package com.spotifestmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.lufinkey.react.spotify.RNSpotifyPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSpotifyPackage()
+          new MainReactPackage()
       );
     }
 
