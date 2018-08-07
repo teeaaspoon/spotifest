421c1c6ec24ea88ae43421e67091b2390fbc84f0
teeaaspoon <tommypoon36@gmail.com>
2018-08-07 11:49:51 -0400

    fixed conflict in login

--- spotifestmobile/App/Components/Login.js
+++ spotifestmobile/App/Components/Login.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { Text, View } from "react-native";
-
-export default class Login extends Component {
-    componentDidMount() {}
-=======
 import { Text, View, NativeModules, TouchableHighlight } from "react-native";
 
 const SpotifyModule = NativeModules.SpotifyModule;
 
 export default class Login extends Component {
     componentDidMount() {
-        console.log(SpotifyModule)
+        console.log(SpotifyModule);
     }
 
     onButtonPress() {
@@ -20,7 +14,6 @@ export default class Login extends Component {
         });
     }
 
->>>>>>> feature/react-native
     render() {
         return (
             <View>
