5ac8131ae96cb3830272b9c9b47bb50b7f3be5bb
teeaaspoon <tommypoon36@gmail.com>
2018-08-08 14:53:49 -0400

    fixed admin page routing to only allow admin to enter

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -4,65 +4,106 @@ import ArtistForm from "./ArtistForm";
 import Request from "./Request";
 import ArtistsToFestivalForm from "./ArtistsToFestivalForm";
 import { connect } from "react-redux";
-import { fetchArtists, fetchFestivals, fetchRequests } from "../../actions/fetchActions";
+import {
+    fetchArtists,
+    fetchFestivals,
+    fetchRequests
+} from "../../actions/fetchActions";
 import { setNav } from "../../actions/adminNavActions";
-
-
+import jwtDecode from "jwt-decode";
 
 class Admin extends Component {
-
-  componentDidMount() {
-    this.props.fetchRequests()
-    this.props.fetchArtists()
-    this.props.fetchFestivals()
-  }
-  handleNav = (e) => {
-    this.props.setNav(e.target.id.slice(0, -3))
-
-  }
-  render() {
-    const allRequests = this.props.requests.map(request => <Request key={request.id} id={request.id} festival={request.festival_name} />)
-    return (
-      <div className="Admin">
-        <p className="spotifestLogo">SPOTI<span>FEST</span></p>
-        <div className="adminNav">
-          <div className="nav">
-            <p onClick={this.handleNav}
-              id="festivalFormNav"
-              className={`${this.props.selectedForm === "festivalForm" ? ("selectedNav") : ("notSelected")} festivalFormNav`}>
-              FESTIVAL FORM
-            </p>
-            <p onClick={this.handleNav}
-              id="artistFormNav"
-              className={`${this.props.selectedForm === "artistForm" ? ("selectedNav") : ("notSelected")} artistFormNav`}>
-              ARTIST FORM
-            </p>
-            <p onClick={this.handleNav}
-              id="artistToFestivalFormNav"
-              className={`${this.props.selectedForm === "artistToFestivalForm" ? ("selectedNav") : ("notSelected")} artistToFestivalFormNav`}>
-              ARTIST TO FESTIVAL FORM
-            </p>
-          </div>
-        </div>
-        <div className="requests">
-          {this.props.requests.length > 0 && <p className="requestHeader">Pending Requests</p>}
-          <div className="allRequests">
-              {allRequests}
-          </div>
-        </div>
-        <FestivalForm />
-        <ArtistForm />
-        <ArtistsToFestivalForm />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.fetchRequests();
+        this.props.fetchArtists();
+        this.props.fetchFestivals();
+    }
+    handleNav = e => {
+        this.props.setNav(e.target.id.slice(0, -3));
+    };
+    render() {
+        let jwt = (jwt = window.localStorage.getItem("jwt"));
+        jwt = jwtDecode(jwt);
+        const allRequests = this.props.requests.map(request => (
+            <Request
+                key={request.id}
+                id={request.id}
+                festival={request.festival_name}
+            />
+        ));
+        return (
+            <div>
+                {jwt && jwt.admin ? (
+                    <div className="Admin">
+                        <p className="spotifestLogo">
+                            SPOTI<span>FEST</span>
+                        </p>
+                        <div className="adminNav">
+                            <div className="nav">
+                                <p
+                                    onClick={this.handleNav}
+                                    id="festivalFormNav"
+                                    className={`${
+                                        this.props.selectedForm ===
+                                        "festivalForm"
+                                            ? "selectedNav"
+                                            : "notSelected"
+                                    } festivalFormNav`}
+                                >
+                                    FESTIVAL FORM
+                                </p>
+                                <p
+                                    onClick={this.handleNav}
+                                    id="artistFormNav"
+                                    className={`${
+                                        this.props.selectedForm === "artistForm"
+                                            ? "selectedNav"
+                                            : "notSelected"
+                                    } artistFormNav`}
+                                >
+                                    ARTIST FORM
+                                </p>
+                                <p
+                                    onClick={this.handleNav}
+                                    id="artistToFestivalFormNav"
+                                    className={`${
+                                        this.props.selectedForm ===
+                                        "artistToFestivalForm"
+                                            ? "selectedNav"
+                                            : "notSelected"
+                                    } artistToFestivalFormNav`}
+                                >
+                                    ARTIST TO FESTIVAL FORM
+                                </p>
+                            </div>
+                        </div>
+                        <div className="requests">
+                            {this.props.requests.length > 0 && (
+                                <p className="requestHeader">
+                                    Pending Requests
+                                </p>
+                            )}
+                            <div className="allRequests">{allRequests}</div>
+                        </div>
+                        <FestivalForm />
+                        <ArtistForm />
+                        <ArtistsToFestivalForm />
+                    </div>
+                ) : (
+                    <div>
+                        <h1>403 Forbidden</h1>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 const mapStateToProps = state => ({
-  requests: state.fetch.requests,
-  selectedForm: state.adminNav.selectedForm
+    requests: state.fetch.requests,
+    selectedForm: state.adminNav.selectedForm
 });
 
 export default connect(
-  mapStateToProps,
-  { fetchArtists, fetchFestivals, fetchRequests, setNav }
+    mapStateToProps,
+    { fetchArtists, fetchFestivals, fetchRequests, setNav }
 )(Admin);
--- client/src/components/admin/FestivalForm.js
+++ client/src/components/admin/FestivalForm.js
@@ -75,38 +75,76 @@ class FestivalForm extends Component {
     render() {
         return (
             <div className="adminForm festivalForm">
-                {this.props.selectedForm === "festivalForm" &&
-                <div className="formContents">
-                    <div className="formTitleAndStatus">
-                        <h1 className="formTitle">Festival</h1>
-                        <p>{this.state.submitStatus}</p>
+                {this.props.selectedForm === "festivalForm" && (
+                    <div className="formContents">
+                        <div className="formTitleAndStatus">
+                            <h1 className="formTitle">Festival</h1>
+                            <p>{this.state.submitStatus}</p>
+                        </div>
+                        <form onSubmit={this.onSubmit}>
+                            <input
+                                placeholder="title"
+                                name="title"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.title}
+                            />
+                            <input
+                                onFocus={e => (e.currentTarget.type = "date")}
+                                onBlur={e => (e.currentTarget.type = "text")}
+                                placeholder="start date"
+                                type="text"
+                                name="start_date"
+                                onChange={this.handleChange}
+                            />
+                            <input
+                                onFocus={e => (e.currentTarget.type = "date")}
+                                onBlur={e => (e.currentTarget.type = "text")}
+                                placeholder="end date"
+                                type="text"
+                                name="end_date"
+                                onChange={this.handleChange}
+                            />
+                            <input
+                                placeholder="city"
+                                name="city"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.city}
+                            />
+                            <input
+                                placeholder="country"
+                                name="country"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.country}
+                            />
+                            <input
+                                placeholder="continent"
+                                name="continent"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.continent}
+                            />
+                            <input
+                                placeholder="longitude"
+                                name="longitude"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.longitude}
+                            />
+                            <input
+                                placeholder="latitude"
+                                name="latitude"
+                                onChange={this.handleChange}
+                                autoComplete="off"
+                                value={this.state.latitude}
+                            />
+                            <br />
+                            <button> submit </button>
+                        </form>
                     </div>
-                    <form onSubmit={this.onSubmit}>
-                        <input placeholder="title" name="title" onChange={this.handleChange} autocomplete="off" value={this.state.title}/>
-                        <input
-                            onFocus={(e) => e.currentTarget.type = "date"}
-                            onBlur={(e) => e.currentTarget.type = "text"}
-                            placeholder="start date"
-                            type="text"
-                            name="start_date"
-                            onChange={this.handleChange}/>
-                        <input
-                            onFocus={(e) => e.currentTarget.type = "date"}
-                            onBlur={(e) => e.currentTarget.type = "text"}
-                            placeholder="end date"
-                            type="text"
-                            name="end_date"
-                            onChange={this.handleChange} />
-                        <input placeholder="city" name="city" onChange={this.handleChange} autocomplete="off" value={this.state.city}/>
-                        <input placeholder="country" name="country" onChange={this.handleChange} autocomplete="off" value={this.state.country}/>
-                        <input placeholder="continent" name="continent" onChange={this.handleChange} autocomplete="off" value={this.state.continent}/>
-                        <input placeholder="longitude" name="longitude" onChange={this.handleChange} autocomplete="off" value={this.state.longitude}/>
-                        <input placeholder="latitude" name="latitude" onChange={this.handleChange} autocomplete="off" value={this.state.latitude} />
-                        <br />
-                        <button> submit </button>
-                    </form>
-                </div>
-                }
+                )}
             </div>
         );
     }
