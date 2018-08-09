93641b7b49fa1e8c741d0c06c3a8f98ad0103f06
teeaaspoon <tommypoon36@gmail.com>
2018-08-09 11:40:29 -0400

    fixed year to show in order

--- client/src/components/homePage/YearSelect.js
+++ client/src/components/homePage/YearSelect.js
@@ -1,36 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Select from 'react-select';
-import { saveYear } from '../../actions/mapActions.js'
-
-
+import Select from "react-select";
+import { saveYear } from "../../actions/mapActions.js";
 
 class YearSelect extends Component {
-  handleYearChange = (selectedOption) => {
-    this.props.saveYear(selectedOption.value)
-  }
+    handleYearChange = selectedOption => {
+        this.props.saveYear(selectedOption.value);
+    };
 
-
-  render() {
-    const years = this.props.festivals.map(festival => festival.title.slice(-4))
-                              .filter((elem, pos, arr) => {
-                                return arr.indexOf(elem) === pos;
-                              });
-    return (
-      <Select
-        className="col-md-4 select-year-container"
-        placeholder="Select year..."
-        classNamePrefix="select-year"
-        value={this.props.year}
-        onChange={this.handleYearChange}
-        options={years.map(year => ({value: year, label: year}))}
-      />
-
-    )
-
-  }
-
-
+    render() {
+        const years = this.props.festivals
+            .map(festival => festival.title.slice(-4))
+            .filter((elem, pos, arr) => {
+                return arr.indexOf(elem) === pos;
+            });
+        const sortedYears = years.sort().reverse();
+        return (
+            <Select
+                className="col-md-4 select-year-container"
+                placeholder="Select year..."
+                classNamePrefix="select-year"
+                value={this.props.year}
+                onChange={this.handleYearChange}
+                options={sortedYears.map(year => ({
+                    value: year,
+                    label: year
+                }))}
+            />
+        );
+    }
 }
 
 const mapStateToProps = state => ({
--- spotifestmobile/App/reducers/filterFestivalReducer.js
+++ spotifestmobile/App/reducers/filterFestivalReducer.js
@@ -12,94 +12,135 @@ const initialState = {
 };
 
 const availableFilters = {
-  year: (festivals, year) => festivals.filter(festival => festival.title.slice(-4) === year),
-  continent: (festivals, continent) => festivals.filter(festival => festival.continent === continent),
-  search: (festivals, searchInput) => {
-    const re = new RegExp(searchInput, "i")
-    const filteredFests = festivals.filter(festival => re.test(festival.title))
-    return filteredFests
-  },
-}
+    year: (festivals, year) =>
+        festivals.filter(festival => festival.title.slice(-4) === year),
+    continent: (festivals, continent) =>
+        festivals.filter(festival => festival.continent === continent),
+    search: (festivals, searchInput) => {
+        const re = new RegExp(searchInput, "i");
+        const filteredFests = festivals.filter(festival =>
+            re.test(festival.title)
+        );
+        return filteredFests;
+    }
+};
 
 const filterFestivals = (allFestivals, filters) => {
-  return filters.reduce( (filteredFestivals, filter) => {
-    const {type, args} = filter
-    return availableFilters[type](filteredFestivals, args)
-  }, allFestivals)
-}
+    return filters.reduce((filteredFestivals, filter) => {
+        const { type, args } = filter;
+        return availableFilters[type](filteredFestivals, args);
+    }, allFestivals);
+};
 
-const getYearsOptions = (festivalList) => {
-  const years = festivalList.map(festival => festival.title.slice(-4))
-                    .filter((elem, pos, arr) => {
-                      return arr.indexOf(elem) === pos;
-                    });
-  return years.concat("cancel")
-}
+const getYearsOptions = festivalList => {
+    const years = festivalList
+        .map(festival => festival.title.slice(-4))
+        .filter((elem, pos, arr) => {
+            return arr.indexOf(elem) === pos;
+        });
+    const sortedYears = years.sort().reverse();
+    return sortedYears.concat("cancel");
+};
 
 export default function(state = initialState, action) {
-  switch (action.type) {
-    case SAVE_YEAR:
-      if (action.payload) {
-        if (state.filters.filter(f => f.type === "year").length > 0) {
-          let filters = state.filters.filter( f => f.type !== "year")
-          filters = [...filters, {type: "year", args: action.payload}]
-          const filteredFestivals = filterFestivals(state.allFestivals, filters)
-          return {...state, filteredFestivals, filters}
-        }
-        const filters = [...state.filters, {type: "year", args: action.payload}]
-        const filteredFestivals = filterFestivals(state.allFestivals, filters)
-        return {...state, filteredFestivals, filters}
-      } else {
-        const filters = state.filters.filter( f => f.type !== "year")
-        const filteredFestivals = filterFestivals(state.allFestivals, filters)
-        return {...state, filteredFestivals, filters}
-      }
-    // case SAVE_CONTINENT:
-    //   if (action.payload) {
-    //     if (state.filters.filter(f => f.type === "continent").length > 0) {
-    //       let filters = state.filters.filter( f => f.type !== "continent")
-    //       filters = [...filters, {type: "continent", args: action.payload}]
-    //       const filteredFestivals = filterFestivals(state.allFestivals, filters)
-    //       return {...state, filteredFestivals, filters}
-    //     }
-    //     const filters = [...state.filters, {type: "continent", args: action.payload}]
-    //     const filteredFestivals = filterFestivals(state.allFestivals, filters)
-    //     return {...state, filteredFestivals, filters}
-    //   } else {
-    //     const filters = state.filters.filter( f => f.type !== "continent")
-    //     const filteredFestivals = filterFestivals(state.allFestivals, filters)
-    //     return {...state, filteredFestivals, filters}
-    //   }
-    case SAVE_SEARCH_INPUT:
-      if (action.payload) {
-        if (state.filters.filter(f => f.type === "search").length > 0) {
-          let filters = state.filters.filter(f=> f.type !== "search")
-          filters = [...filters, {type: "search", args: action.payload}]
-          const filteredFestivals = filterFestivals(state.allFestivals, filters)
-          const yearOptions = getYearsOptions(filteredFestivals)
-          return {...state, filteredFestivals, filters, yearOptions}
-        }
-        const filters = [...state.filters, {type: "search", args: action.payload}]
-        const filteredFestivals = filterFestivals(state.allFestivals, filters)
-        const yearOptions = getYearsOptions(filteredFestivals)
-        return {...state, filteredFestivals, filters, yearOptions}
-      } else {
-        const filters = state.filters.filter( f => f.type !== "search")
-        const filteredFestivals = filterFestivals(state.allFestivals, filters)
-        const yearOptions = getYearsOptions(filteredFestivals)
-        return {...state, filteredFestivals, filters, yearOptions}
-      }
-    case SELECT_ALL_FESTIVALS:
-      const yearOptions = getYearsOptions(action.payload)
-      return {
-          ...state,
-          allFestivals: action.payload,
-          filteredFestivals: action.payload,
-          yearOptions: yearOptions
-      }
-    default:
-      return state;
-  }
-
-
+    switch (action.type) {
+        case SAVE_YEAR:
+            if (action.payload) {
+                if (state.filters.filter(f => f.type === "year").length > 0) {
+                    let filters = state.filters.filter(f => f.type !== "year");
+                    filters = [
+                        ...filters,
+                        { type: "year", args: action.payload }
+                    ];
+                    const filteredFestivals = filterFestivals(
+                        state.allFestivals,
+                        filters
+                    );
+                    return { ...state, filteredFestivals, filters };
+                }
+                const filters = [
+                    ...state.filters,
+                    { type: "year", args: action.payload }
+                ];
+                const filteredFestivals = filterFestivals(
+                    state.allFestivals,
+                    filters
+                );
+                return { ...state, filteredFestivals, filters };
+            } else {
+                const filters = state.filters.filter(f => f.type !== "year");
+                const filteredFestivals = filterFestivals(
+                    state.allFestivals,
+                    filters
+                );
+                return { ...state, filteredFestivals, filters };
+            }
+        // case SAVE_CONTINENT:
+        //   if (action.payload) {
+        //     if (state.filters.filter(f => f.type === "continent").length > 0) {
+        //       let filters = state.filters.filter( f => f.type !== "continent")
+        //       filters = [...filters, {type: "continent", args: action.payload}]
+        //       const filteredFestivals = filterFestivals(state.allFestivals, filters)
+        //       return {...state, filteredFestivals, filters}
+        //     }
+        //     const filters = [...state.filters, {type: "continent", args: action.payload}]
+        //     const filteredFestivals = filterFestivals(state.allFestivals, filters)
+        //     return {...state, filteredFestivals, filters}
+        //   } else {
+        //     const filters = state.filters.filter( f => f.type !== "continent")
+        //     const filteredFestivals = filterFestivals(state.allFestivals, filters)
+        //     return {...state, filteredFestivals, filters}
+        //   }
+        case SAVE_SEARCH_INPUT:
+            if (action.payload) {
+                if (state.filters.filter(f => f.type === "search").length > 0) {
+                    let filters = state.filters.filter(
+                        f => f.type !== "search"
+                    );
+                    filters = [
+                        ...filters,
+                        { type: "search", args: action.payload }
+                    ];
+                    const filteredFestivals = filterFestivals(
+                        state.allFestivals,
+                        filters
+                    );
+                    const yearOptions = getYearsOptions(filteredFestivals);
+                    return {
+                        ...state,
+                        filteredFestivals,
+                        filters,
+                        yearOptions
+                    };
+                }
+                const filters = [
+                    ...state.filters,
+                    { type: "search", args: action.payload }
+                ];
+                const filteredFestivals = filterFestivals(
+                    state.allFestivals,
+                    filters
+                );
+                const yearOptions = getYearsOptions(filteredFestivals);
+                return { ...state, filteredFestivals, filters, yearOptions };
+            } else {
+                const filters = state.filters.filter(f => f.type !== "search");
+                const filteredFestivals = filterFestivals(
+                    state.allFestivals,
+                    filters
+                );
+                const yearOptions = getYearsOptions(filteredFestivals);
+                return { ...state, filteredFestivals, filters, yearOptions };
+            }
+        case SELECT_ALL_FESTIVALS:
+            const yearOptions = getYearsOptions(action.payload);
+            return {
+                ...state,
+                allFestivals: action.payload,
+                filteredFestivals: action.payload,
+                yearOptions: yearOptions
+            };
+        default:
+            return state;
+    }
 }
